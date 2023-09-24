f086080119a9525fb9a65198ac76de05b44f6d1d
ss-karthik <maniac.sskarthik@gmail.com>
2023-09-24 05:30:01 +0530

    Solve Exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, n) {
+    let newstr = "";
+    if (n < 0) {
+        return "ERROR";
+    }
 
+    for (let i = 0; i < n; i++) {
+        newstr += string;
+    }
+    return newstr;
 };
 
 // Do not edit below this line
