4ac51f193d058cceab677781419cbff0c545eb00
ss-karthik <maniac.sskarthik@gmail.com>
2023-10-05 18:19:27 +0530

    Solve 8 and 9 exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let totalSum = 0;
+	arr.forEach(element => {
+    totalSum += element;
+  });
+
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+	arr.forEach(element => {
+    product *= element;
+  });
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return Math.pow(a, b);
 };
 
-const factorial = function() {
+const factorial = function(n) {
+  if (n === 0) {
+    return 1;
+  }else {
+    return n * factorial(n-1);
+  }
 	
 };
 
