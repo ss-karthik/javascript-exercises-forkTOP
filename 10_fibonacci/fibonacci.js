f4a81c620e7cf42402373bc348da32157389c9a1
ss-karthik <maniac.sskarthik@gmail.com>
2023-10-05 18:49:14 +0530

    Solve 9 - fibonacci series

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 0) {
+        return "OOPS";
+    }
+    if (n == 0) {
+        return 0;
+    }
+    else if (n == 1) {
+        return 1;
+    }
 
+    p1 = 0;
+    p2 = 1;
+    let currentValue = 0;
+
+    for (let i = 1; i < n; i++) {
+        currentValue = p1+p2;
+        p1 = p2;
+        p2 = currentValue;
+    }
+
+    return currentValue;
 };
 
 // Do not edit below this line
